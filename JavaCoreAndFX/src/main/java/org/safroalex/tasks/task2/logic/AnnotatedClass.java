8c2e024ca4d4cbad058736c89741fa012432e73f
safroalex <alexsafr.a@ya.ru>
2023-11-05 23:46:41 +0300

    add comments

--- JavaCoreAndFX/src/main/java/org/safroalex/tasks/task2/logic/AnnotatedClass.java
+++ JavaCoreAndFX/src/main/java/org/safroalex/tasks/task2/logic/AnnotatedClass.java
@@ -1,29 +1,53 @@
 package org.safroalex.tasks.task2.logic;
 
+/**
+ * Класс, содержащий методы с аннотацией @Repeat для демонстрации
+ * возможности повторного выполнения метода заданное количество раз.
+ */
 public class AnnotatedClass {
 
-    // Простейший публичный метод
+    /**
+     * Простой публичный метод без параметров и аннотаций.
+     */
     public void simplePublicMethod() {
         System.out.println("This is a simple public method.");
     }
 
-    // Защищенные методы с аннотациями и одним параметром
+    /**
+     * Защищенный метод, аннотированный для двукратного выполнения.
+     * Принимает один параметр типа int.
+     * @param a целочисленное значение для демонстрации.
+     */
     @Repeat(times = 2)
     protected void protectedMethodWithInt(int a) {
         System.out.println("This is a protected method with int: " + a);
     }
 
+    /**
+     * Защищенный метод с использованием аннотации @Repeat со значением по умолчанию.
+     * Принимает один параметр типа double.
+     * @param b значение с плавающей точкой для демонстрации.
+     */
     @Repeat  // использование значения по умолчанию
     protected void protectedMethodWithDouble(double b) {
         System.out.println("This is a protected method with double: " + b);
     }
 
-    // Приватные методы с аннотациями и одним параметром
+    /**
+     * Приватный метод, аннотированный для трехкратного выполнения.
+     * Принимает один параметр типа String.
+     * @param s строка для демонстрации.
+     */
     @Repeat(times = 3)
     private void privateMethodWithString(String s) {
         System.out.println("This is a private method with String: " + s);
     }
 
+    /**
+     * Приватный метод, аннотированный для трехкратного выполнения.
+     * Принимает один параметр типа Class<?>.
+     * @param c класс-параметр для демонстрации использования рефлексии.
+     */
     @Repeat(times = 3)
     private void privateMethodWithClass(Class<?> c) {
         System.out.println("This is a private method with Class: " + c.getName());
--- JavaCoreAndFX/src/main/java/org/safroalex/tasks/task5/logic/Methods.java
+++ JavaCoreAndFX/src/main/java/org/safroalex/tasks/task5/logic/Methods.java
@@ -6,55 +6,95 @@ import java.util.stream.Collectors;
 
 public class Methods {
 
+    /**
+     * Вычисляет среднее значение списка целых чисел.
+     *
+     * @param list Список целых чисел для обработки.
+     * @return Среднее значение чисел в списке.
+     */
     public static double method1(List<Integer> list) {
         return list
                 .stream()
-                .mapToInt(Integer::intValue)    // mapToInt-converting a wrapper into a primitive stream
+                .mapToInt(Integer::intValue)
                 .summaryStatistics()
                 .getAverage();
     }
 
+    /**
+     * Преобразует список строк в верхний регистр и добавляет префикс "new" к каждой строке.
+     *
+     * @param list Список строк для обработки.
+     * @return Обновленный список строк.
+     */
     public static List<String> method2(List<String> list) {
         return list
                 .stream()
-                .map(String::toUpperCase)       // map-give us opportunity to create function to changing
-                .map(s -> "new" + s)            // each element and skipping it further
+                .map(String::toUpperCase)
+                .map(s -> "new" + s)
                 .toList();
     }
 
+    /**
+     * Возвращает список уникальных чисел, возведенных в квадрат.
+     *
+     * @param list Список целых чисел для обработки.
+     * @return Список квадратов уникальных чисел.
+     */
     public static List<Integer> method3(List<Integer> list) {
-        return list.stream()                                        // skipping elements further if
-                .filter(n -> Collections.frequency(list, n) == 1)   // they consider the "frequency" definition
-                .map(n -> n * n)                                    // Collections.frequency-how many times does
-                .toList();                                          // element n occur in the list
+        return list.stream()
+                .filter(n -> Collections.frequency(list, n) == 1)
+                .map(n -> n * n)
+                .toList();
     }
 
+    /**
+     * Фильтрует коллекцию строк, возвращая те, что начинаются с заданной буквы, в отсортированном порядке.
+     *
+     * @param list Коллекция строк для обработки.
+     * @param firstLetter Первая буква для фильтрации строк.
+     * @return Отфильтрованный и отсортированный список строк.
+     */
     public static List<String> method4(Collection<String> list, String firstLetter) {
         return list.stream()
                 .filter(f -> f.startsWith(firstLetter))
-                .sorted(String.CASE_INSENSITIVE_ORDER)               // CASE_INSENSITIVE_ORDER-sort independent
-                .toList();                                           // of register
+                .sorted(String.CASE_INSENSITIVE_ORDER)
+                .toList();
     }
 
-    public static <T> T method5(Collection<T> list) {                // T is a generic with auto-indication of the type,
-        // but you can also specify it yourself
+    /**
+     * Возвращает последний элемент из потока элементов.
+     *
+     * @param <T> Тип элементов в коллекции.
+     * @param list Коллекция элементов.
+     * @return Последний элемент коллекции.
+     * @throws NoSuchElementException Если коллекция пуста.
+     */
+    public static <T> T method5(Collection<T> list) {
         return list.stream()
-                .reduce((a, b) -> b)             /*passes through the elements of the collection and performs the operation
-                                                    to the right of the arrow, i.e. points to the last element in our case*/
-                .orElseThrow(() -> new NoSuchElementException("Collection is empty")); //raise an exception
-        // if reduce cannot return the result
+                .reduce((a, b) -> b)
+                .orElseThrow(() -> new NoSuchElementException("Collection is empty"));
     }
 
+    /**
+     * Суммирует все четные числа в массиве.
+     *
+     * @param array Массив целых чисел для обработки.
+     * @return Сумма четных чисел в массиве.
+     */
     public static int method6(int[] array) {
         return Arrays.stream(array)
                 .filter(i -> i % 2 == 0)
                 .reduce(0, Integer::sum);
     }
 
+    /**
+     * Создает карту, в которой каждая строка отображается на символ, являющийся ее первым символом.
+     *
+     * @param list Список строк для создания карты.
+     * @return Карта с первыми символами строк в качестве ключей и оставшейся частью строки в качестве значений.
+     */
     public static Map<Character, String> method7 (List<String> list) {
-        return list
-                .stream()
-                .collect(Collectors                 //collect-convert to collection
+        return list.stream().collect(Collectors
                         .toMap(str -> str.charAt(0), str -> str.substring(1)));
     }
 }
